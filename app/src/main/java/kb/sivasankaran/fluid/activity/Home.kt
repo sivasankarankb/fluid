d9880c06b42b9badcee5f80131d6b2fa502b2520
Sivasankaran K B <kbsiva7@gmail.com>
2020-10-19 10:00:39 +0530

    Added home (main) activity.

---
+++ app/src/main/java/kb/sivasankaran/fluid/activity/Home.kt
@@ -0,0 +1,12 @@
+package kb.sivasankaran.fluid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kb.sivasankaran.fluid.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
