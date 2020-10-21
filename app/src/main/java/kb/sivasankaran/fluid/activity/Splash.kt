3c533ecc01cf133a75d9d385eaa96d1da1f39e14
Sivasankaran K B <kbsiva7@gmail.com>
2020-10-21 09:08:57 +0530

    Splash activity launches home activity after 2 seconds.

--- app/src/main/java/kb/sivasankaran/fluid/activity/Splash.kt
+++ app/src/main/java/kb/sivasankaran/fluid/activity/Splash.kt
@@ -1,13 +1,23 @@
 package kb.sivasankaran.fluid.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kb.sivasankaran.fluid.R
+import java.util.*
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+
+        Timer().schedule(object: TimerTask(){
+            override fun run() {
+                intent = Intent(this@Splash, Home::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        },2000)
     }
 }
