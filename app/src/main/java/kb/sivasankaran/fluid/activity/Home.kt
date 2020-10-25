d431e39df63531968408ff5783cf8364755b90f8
Sivasankaran K B <kbsiva7@gmail.com>
2020-10-25 22:51:18 +0530

    Added time tick broadcast receiver.
    Receiver is enabled and disabled in the home activity.
    Added vector drawable compatibility library.

--- app/src/main/java/kb/sivasankaran/fluid/activity/Home.kt
+++ app/src/main/java/kb/sivasankaran/fluid/activity/Home.kt
@@ -1,12 +1,29 @@
 package kb.sivasankaran.fluid.activity
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kb.sivasankaran.fluid.R
+import kb.sivasankaran.fluid.receiver.TimeTick
 
 class Home : AppCompatActivity() {
+    val tickReceiver by lazy { TimeTick() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        supportActionBar?.hide()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter(Intent.ACTION_TIME_TICK)
+        registerReceiver(tickReceiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(tickReceiver)
     }
 }
---
+++ app/src/main/java/kb/sivasankaran/fluid/receiver/TimeTick.kt
@@ -0,0 +1,13 @@
+package kb.sivasankaran.fluid.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class TimeTick : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("TimeTick.onReceive() is not implemented")
+    }
+}
