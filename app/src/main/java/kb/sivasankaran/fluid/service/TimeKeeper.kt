a48284a88d8678a54d702c999f0dd95895e680cd
Sivasankaran K B <kbsiva7@gmail.com>
2020-11-01 20:34:49 +0530

    Added time keeper service that does nothing yet.

---
+++ app/src/main/java/kb/sivasankaran/fluid/service/TimeKeeper.kt
@@ -0,0 +1,14 @@
+package kb.sivasankaran.fluid.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+
+class TimeKeeper : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+}
