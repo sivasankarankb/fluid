d6db31114181aac318e72044e0a529c0f487cfda
Sivasankaran K B <kbsiva7@gmail.com>
2020-11-04 08:22:05 +0530

    Added schedule list activity and add icon.
    A recycler view displays the schedule list.
    An action button is used to add items.

---
+++ app/src/main/java/kb/sivasankaran/fluid/activity/ScheduleList.kt
@@ -0,0 +1,12 @@
+package kb.sivasankaran.fluid.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kb.sivasankaran.fluid.R
+
+class ScheduleList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_list)
+    }
+}
