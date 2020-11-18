c055db3e78eebe5cc1e23e5d6c772f16fb84d6f1
Sivasankaran K B <kbsiva7@gmail.com>
2020-11-18 22:55:12 +0530

    Show error message if fields empty.
    Fixed cropped fab shadow by using margin.

--- app/src/main/java/kb/sivasankaran/fluid/dialog/ScheduleTask.kt
+++ app/src/main/java/kb/sivasankaran/fluid/dialog/ScheduleTask.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.DialogFragment
 import kb.sivasankaran.fluid.R
+import kb.sivasankaran.fluid.databinding.DialogScheduleTaskBinding
 
 class ScheduleTask: DialogFragment() {
 
@@ -20,13 +23,29 @@ class ScheduleTask: DialogFragment() {
     val taskInfo = TaskInfo()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val viewRoot = inflater.inflate(R.layout.dialog_schedule_task, container, true)
+
+        val binding = DialogScheduleTaskBinding.inflate(layoutInflater)
         
-        viewRoot.findViewById<Button>(R.id.add_button).setOnClickListener {
-            taskInfo.description = viewRoot.findViewById<EditText>(R.id.task_label).text.toString()
-            taskInfo.start = viewRoot.findViewById<EditText>(R.id.task_start).text.toString()
+        binding.addButton.setOnClickListener{
+            val exclamationMark = ContextCompat.getDrawable(inflater.context, R.drawable.ic_exclamation)
+
+            taskInfo.description = binding.taskLabel.text.toString()
+
+            if(taskInfo.description.isEmpty()) {
+                binding.taskLabel.setError("Can't be empty!", exclamationMark)
+                binding.taskLabel.requestFocus()
+                return@setOnClickListener
+            }
+
+            taskInfo.start = binding.taskStart.text.toString()
+
+            if(taskInfo.start.isEmpty()) {
+                binding.taskStart.setError("Can't be empty!", exclamationMark)
+                binding.taskStart.requestFocus()
+                return@setOnClickListener
+            }
         }
         
-        return viewRoot
+        return binding.root
     }
 }
